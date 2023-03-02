c9cd61a05759b86ac67128ee6da413e155656ac9
Fernando Vargas <fvargas320@gmail.com>
2023-03-02 01:28:52 -0800

    changed gridpage to a stateful page

--- lib/Pages/CocktailPages/grid_cocktails_page.dart
+++ lib/Pages/CocktailPages/grid_cocktails_page.dart
@@ -1,15 +1,19 @@
-import 'package:drinkly_cocktails/data_model/cocktail.dart';
 import 'package:flutter/material.dart';
 import 'package:page_transition/page_transition.dart';
-import 'dart:convert';
+
+import '../../data_model/cocktail.dart';
 import 'cocktail_card_page.dart';
 
-class GridCocktails extends StatelessWidget {
+class GridCocktails extends StatefulWidget {
   final List<Cocktail> cocktail_list;
-
   const GridCocktails({Key? key, required this.cocktail_list})
       : super(key: key);
 
+  @override
+  State<GridCocktails> createState() => _GridCocktailsState();
+}
+
+class _GridCocktailsState extends State<GridCocktails> {
   @override
   Widget build(BuildContext context) => Scaffold(
         appBar: PreferredSize(
@@ -23,18 +27,16 @@ class GridCocktails extends StatelessWidget {
             elevation: 5,
           ),
         ),
-        body: Container(
-          child: GridView.builder(
-            gridDelegate:
-                SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
-            padding: EdgeInsets.all(14),
-            scrollDirection: Axis.vertical,
-            itemCount: cocktail_list.length,
-            //itemBuilder: (context, index) => buildCard(),
-            itemBuilder: (context, index) => Padding(
-              padding: const EdgeInsets.all(5.0),
-              child: buildCard(index),
-            ),
+        body: GridView.builder(
+          gridDelegate:
+              SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
+          padding: EdgeInsets.all(14),
+          scrollDirection: Axis.vertical,
+          itemCount: widget.cocktail_list.length,
+          //itemBuilder: (context, index) => buildCard(),
+          itemBuilder: (context, index) => Padding(
+            padding: const EdgeInsets.all(5.0),
+            child: buildCard(index),
           ),
         ),
       );
@@ -53,16 +55,17 @@ class GridCocktails extends StatelessWidget {
                 aspectRatio: 4 / 3,
                 child: Material(
                   child: Ink.image(
-                    image: NetworkImage(cocktail_list[index].strDrinkThumb),
+                    image:
+                        NetworkImage(widget.cocktail_list[index].strDrinkThumb),
                     fit: BoxFit.cover,
                     child: InkWell(
                       onTap: () {
-                        //   Navigator.push(
-                        //       context,
-                        //       PageTransition(
-                        //           type: PageTransitionType.fade,
-                        //           child: CocktailCardPage(
-                        //               cocktail: cocktail_list[index])));
+                        Navigator.push(
+                            context,
+                            PageTransition(
+                                type: PageTransitionType.fade,
+                                child: CocktailCardPage(
+                                    cocktail: widget.cocktail_list[index])));
                       },
                     ),
                   ),
@@ -71,7 +74,7 @@ class GridCocktails extends StatelessWidget {
             ), //Pic
 
             Text(
-              cocktail_list[index].strDrink,
+              widget.cocktail_list[index].strDrink,
               style: const TextStyle(
                   fontWeight: FontWeight.bold,
                   fontSize: 18,
@@ -81,74 +84,3 @@ class GridCocktails extends StatelessWidget {
         ),
       );
 }
-
-// import 'package:drinkly_cocktails/data_model/cocktail.dart';
-// import 'package:flutter/material.dart';
-// import 'package:page_transition/page_transition.dart';
-//
-// class GridCocktails extends StatelessWidget {
-//   final List<Cocktail> cocktails;
-//
-//   const GridCocktails({Key? key, required this.cocktails}) : super(key: key);
-//
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: PreferredSize(
-//         preferredSize: const Size.fromHeight(50),
-//         child: AppBar(
-//           title: Text("Grid View"),
-//           backgroundColor: Colors.grey.shade800,
-//           elevation: 5,
-//         ),
-//       ),
-//       body: GridView.builder(
-//         gridDelegate:
-//         SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
-//         padding: EdgeInsets.all(14),
-//         scrollDirection: Axis.vertical,
-//         itemCount: cocktails.length,
-//         //itemBuilder: (context, index) => buildCard(),
-//         itemBuilder: (context, index) => Padding(
-//           padding: const EdgeInsets.all(5.0),
-//           child: buildCard(index),
-//         ),
-//       ),
-//     );
-//   }
-//
-//   Widget buildCard(int index) => Container(
-//     decoration: BoxDecoration(
-//         border: Border.all(color: Colors.blue),
-//         color: Colors.blue,
-//         borderRadius: BorderRadius.circular(20)),
-//     //height: 200,
-//
-//     child: Column(
-//       children: [
-//         Expanded(
-//           child: AspectRatio(
-//             aspectRatio: 4 / 3,
-//             child: Material(
-//               child: Ink.image(
-//                 image: NetworkImage(cocktails[index].strDrinkThumb),
-//                 fit: BoxFit.cover,
-//                 child: InkWell(
-//                   onTap: () {},
-//                 ),
-//               ),
-//             ),
-//           ),
-//         ), //Pic
-//
-//         Text(
-//           cocktails[index].strDrink,
-//           style: const TextStyle(
-//               fontWeight: FontWeight.bold,
-//               fontSize: 18,
-//               color: Colors.black87),
-//         ), //Cocktail Name
-//       ],
-//     ),
-//   );
-// }
