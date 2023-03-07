f82c2a8b5c77ff1fa87e15a2dafbe00918469587
Fernando Vargas <fvargas320@gmail.com>
2023-03-06 21:13:26 -0800

    added google service file

--- lib/main.dart
+++ lib/main.dart
@@ -4,14 +4,15 @@ import 'package:flutter/material.dart';
 
 import 'Pages/homepage.dart';
 import 'package:firebase_core/firebase_core.dart';
+
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp ({Key?  key}) : super(key: key);
+  const MyApp({Key? key}) : super(key: key);
 
   // This widget is the root of your application.
   @override
