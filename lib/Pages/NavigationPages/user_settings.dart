5d8f72ae54fa599a0b2ee91bed9e158f723105d9
krishnaCsun <krishna-kanth.sattiraju.621@my.csun.edu>
2023-03-07 20:00:41 -0800

    Local Settings Changes

--- lib/Pages/NavigationPages/user_settings.dart
+++ lib/Pages/NavigationPages/user_settings.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_settings_screens/flutter_settings_screens.dart';
 
 class SettingsPage extends StatefulWidget {
   const SettingsPage({Key? key}) : super(key: key);
