9aa1cdd95f17112c4ddd3e3523faa8d97861b0e6
krishnaCsun <krishna-kanth.sattiraju.621@my.csun.edu>
2023-04-11 19:08:04 -0700

    Logout Implementation

--- lib/Pages/NavigationPages/user_settings.dart
+++ lib/Pages/NavigationPages/user_settings.dart
@@ -41,7 +41,7 @@ class _SettingsPageState extends State<SettingsPage> {
           ListTile(
             title: Text('Sign Out'),
             onTap: () {
-              // TODO: Implement sign out functionality
+              logout();
             },
           ),
         ],
@@ -52,43 +52,11 @@ class _SettingsPageState extends State<SettingsPage> {
 
 
 
-
-//
-//
-// class SettingsPage extends StatelessWidget {
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(
-//         title: Text('Settings'),
-//       ),
-//       body: ListView(
-//         children: <Widget>[
-//           UserAccountsDrawerHeader(
-//             accountName: Text('John Doe'),
-//             accountEmail: Text('johndoe@example.com'),
-//             currentAccountPicture: CircleAvatar(
-//               backgroundImage: AssetImage('assets/profile_pic.png'),
-//             ),
-//           ),
-//           ListTile(
-//             title: Text('App Version'),
-//             subtitle: Text('1.0.0'),
-//           ),
-//           ListTile(
-//             title: Text('Add New List'),
-//             onTap: () {
-//               // TODO: Implement add new list functionality
-//             },
-//           ),
-//           ListTile(
-//             title: Text('Sign Out'),
-//             onTap: () {
-//               // TODO: Implement sign out functionality
-//             },
-//           ),
-//         ],
-//       ),
-//     );
-//   }
-// }
+// Call this function to log the user out
+Future<void> logout() async {
+  try {
+    await FirebaseAuth.instance.signOut();
+  } catch (e) {
+    print('Error logging out: $e');
+  }
+}
